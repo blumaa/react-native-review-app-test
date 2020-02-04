fa06203925b690c8165e36dcfada59a6bbb3d6ab
Aaron Blum <blumaa@gmail.com>
2020-02-04 15:45:37 +0100

    added rating system

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,20 +1,34 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-import { globalStyles } from "../styles/global";
-import Card from '../shared/card';
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+import { globalStyles, images } from "../styles/global";
+import Card from "../shared/card";
 
-
-function ReviewDetails( { navigation } ) {
+function ReviewDetails({ navigation }) {
+  const rating = navigation.getParam("rating");
 
   return (
     <View style={globalStyles.container}>
       <Card>
-        <Text>{ navigation.getParam('title')}</Text>
-        <Text>{ navigation.getParam('body')}</Text>
-        <Text>{ navigation.getParam('rating')}</Text>
+        <Text>{navigation.getParam("title")}</Text>
+        <Text>{navigation.getParam("body")}</Text>
+        <View style={styles.rating}>
+          <Text>Reading List rating:</Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
 
 export default ReviewDetails;
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderColor: "#eee"
+  }
+});
