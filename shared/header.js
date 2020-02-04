14161e9e52816524eeddfd3bcb2ac6baf33d2890
Aaron Blum <blumaa@gmail.com>
2020-02-04 15:55:33 +0100

    background image

--- shared/header.js
+++ shared/header.js
@@ -1,31 +1,34 @@
 import React from "react";
-import {Dimensions} from 'react-native';
-import { View, Text, StyleSheet } from "react-native";
+import { Dimensions } from "react-native";
+import { View, Text, StyleSheet, Image, ImageBackground } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 export default function Header({ navigation, title }) {
   const openMenu = () => {
-    navigation.openDrawer()
+    navigation.openDrawer();
   };
   return (
-    <View style={styles.header}>
+    <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
       <MaterialIcons
         name="menu"
         size={28}
         onPress={openMenu}
         style={styles.icon}
       />
-      <View>
+      <View style={styles.headerTitle}>
+        <Image
+          source={require("../assets/heart_logo.png")}
+          style={styles.headerImage}
+        />
         <Text style={styles.headerText}>{title}</Text>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
 
-
 const styles = StyleSheet.create({
   header: {
-    width: Dimensions.get('window').width,
+    width: Dimensions.get("window").width,
     height: "100%",
     flexDirection: "row",
     alignItems: "center",
@@ -41,5 +44,13 @@ const styles = StyleSheet.create({
   icon: {
     position: "absolute",
     left: 16
+  },
+  headerTitle: {
+    flexDirection: "row"
+  },
+  headerImage: {
+    width: 26,
+    height: 26,
+    marginHorizontal: 10
   }
 });
