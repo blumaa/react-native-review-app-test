757ba182eca141a0d95549862b4cc953e9bc73b9
Aaron Blum <blumaa@gmail.com>
2020-02-04 15:20:51 +0100

    added cards to review details

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -8,9 +8,11 @@ function ReviewDetails( { navigation } ) {
 
   return (
     <View style={globalStyles.container}>
-      <Text>{ navigation.getParam('title')}</Text>
-      <Text>{ navigation.getParam('body')}</Text>
-      <Text>{ navigation.getParam('rating')}</Text>
+      <Card>
+        <Text>{ navigation.getParam('title')}</Text>
+        <Text>{ navigation.getParam('body')}</Text>
+        <Text>{ navigation.getParam('rating')}</Text>
+      </Card>
     </View>
   );
 }
